36bb33632a30f9a753749ec568e5107efd7a8235
Christopher Mendoza <tiptop1112@gmail.com>
2025-05-27 12:03:45 -0700

    Installed jest and eslint

---
+++ ship.test.js
@@ -0,0 +1,9 @@
+import { Ship } from "./Ship";
+
+test('testing Ship class', ()=> {
+  const ship = new Ship(3);
+  expect(ship.hit()).toBe(1);
+  expect(ship.hit()).toBe(2);
+  expect(ship.hit()).toBe(3);
+  expect(ship.isSunk()).toBe(true);
+})
