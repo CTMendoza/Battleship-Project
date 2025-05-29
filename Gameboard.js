04e9167125df4928df4e2f5ff7c4a4d70cd5d5d5
Christopher Mendoza <tiptop1112@gmail.com>
2025-05-29 11:03:49 -0700

    Modified placeship method Added logic to the placeship method to check a ship.length and to place it on the board based on the bases on the direction passed as an arguement -- either 'horizontal' or 'vertical'

--- Gameboard.js
+++ Gameboard.js
@@ -20,7 +20,23 @@ class Gameboard {
 
   }
 
-  placeShip(x,y,ship) {
+  placeShip(x,y,ship, direction) {
+    if(ship.length > 1 && direction === 'horizontal') {
+      let i = 0
+      while (i < ship.length) {
+        this.board[x][y + i] = ship
+        i ++
+      }
+      return this.board[x][y]
+    }
+    else if (ship.length > 1 && direction === 'vertical') {
+      let i = 0
+      while (i < ship.length) {
+        this.board[x + i][y] = ship
+        i++
+      }
+      return this.board[x][y]
+    }
     this.board[x][y] = ship
     return this.board[x][y]
   }
--- gameboard.test.js
+++ gameboard.test.js
@@ -1,7 +1,17 @@
 import { Gameboard } from "./Gameboard";
 import { Ship } from "./Ship";
 
-test('testing Gameboard class', () => {
+test('testing Gameboard class ship placement', () => {
   const gameboard = new Gameboard()
-  expect(gameboard.placeShip(0,0,'ship')).toBe('ship')
+  const ship1 = new Ship(3)
+  const ship2 = new Ship(5)
+  expect(gameboard.placeShip(0,0, ship1,'horizontal')).toBe(ship1)
+  expect(gameboard.board[0][1]).toBe(ship1)
+  expect(gameboard.board[0][2]).toBe(ship1)
+  expect(gameboard.placeShip(3, 5, ship2, 'vertical')).toBe(ship2)
+  expect(gameboard.board[3][5]).toBe(ship2)
+  expect(gameboard.board[4][5]).toBe(ship2)
+  expect(gameboard.board[5][5]).toBe(ship2)
+  expect(gameboard.board[6][5]).toBe(ship2)
+  expect(gameboard.board[7][5]).toBe(ship2)
 })
