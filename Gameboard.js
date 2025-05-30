803e56be103bd7b49af3691e9b7ecdc7eadcf38b
Christopher Mendoza <tiptop1112@gmail.com>
2025-05-30 12:24:55 -0700

    Finalized placeShip method of Gameboard class
    Cleaned up and finalized logic for the placeShip method
    so that it correctly places ships on the gameboard
    based on size and whether there are other ships occupying
    the current ships required space. Used jest to test
    these concerns and passed.

--- Gameboard.js
+++ Gameboard.js
@@ -21,24 +21,55 @@ class Gameboard {
   }
 
   placeShip(x,y,ship, direction) {
-    if(ship.length > 1 && direction === 'horizontal') {
+    if((x < 0 || x > 9 ) || (y < 0 || y > 9)) {
+      return 'Error: Either x or y cooridnate is outside the bounds of the board. Please re-enter coordinates'
+    }
+    // if(this.board[x][y] !== null) {
+    //   return `Error: ${this.board[x][y]} already occupys this space`
+    // }
+
+    if( direction === 'horizontal') {
       let i = 0
       while (i < ship.length) {
-        this.board[x][y + i] = ship
+        if (y + i < 0 || y + i > 9) {
+          return 'Error: The y cooridnate is outside the bounds of the board. Please re-enter coordinates'
+        }
+        if (this.board[x][y + i] !== null) {
+          return `Error: A ship already occupies this space`
+        }
         i ++
       }
-      return this.board[x][y]
+
+      i = 0;
+      while (i < ship.length) {
+        this.board[x][y + i] = ship
+        i++
+      }
+
+      return ship
     }
-    else if (ship.length > 1 && direction === 'vertical') {
-      let i = 0
+
+
+    if ( direction === 'vertical') {
+      let i = 0;
+      while (i < ship.length) {
+        if (x + i < 0 || x + i > 9) {
+          return 'Error: The x cooridnate is outside the bounds of the board. Please re-enter coordinates'
+        }
+        if (this.board[x + i][y ] !== null) {
+          return `Error: A ship already occupies this space`
+        }
+        i++
+      }
+
+      i = 0;
       while (i < ship.length) {
         this.board[x + i][y] = ship
         i++
       }
-      return this.board[x][y]
+      return ship
     }
-    this.board[x][y] = ship
-    return this.board[x][y]
+    return 'Error: Direction must be either "horizontal" or "vertical"';
   }
 
 }
--- gameboard.test.js
+++ gameboard.test.js
@@ -5,13 +5,29 @@ test('testing Gameboard class ship placement', () => {
   const gameboard = new Gameboard()
   const ship1 = new Ship(3)
   const ship2 = new Ship(5)
-  expect(gameboard.placeShip(0,0, ship1,'horizontal')).toBe(ship1)
-  expect(gameboard.board[0][1]).toBe(ship1)
-  expect(gameboard.board[0][2]).toBe(ship1)
+  const ship3 =new Ship(1)
+  const ship4 = new Ship(1)
+  const ship5 = new Ship(2)
+  const ship6 = new Ship(2)
+
+  expect(gameboard.placeShip(0, 8, ship1, 'horizontal')).toBe('Error: The y cooridnate is outside the bounds of the board. Please re-enter coordinates')
+  expect(gameboard.board[0][8]).toBe(null)
+  expect(gameboard.board[0][9]).toBe(null)
+
   expect(gameboard.placeShip(3, 5, ship2, 'vertical')).toBe(ship2)
   expect(gameboard.board[3][5]).toBe(ship2)
   expect(gameboard.board[4][5]).toBe(ship2)
   expect(gameboard.board[5][5]).toBe(ship2)
   expect(gameboard.board[6][5]).toBe(ship2)
   expect(gameboard.board[7][5]).toBe(ship2)
+
+  expect(gameboard.placeShip(9, 10, ship3)).toBe('Error: Either x or y cooridnate is outside the bounds of the board. Please re-enter coordinates')
+  expect(gameboard.placeShip(10, 9, ship3)).toBe('Error: Either x or y cooridnate is outside the bounds of the board. Please re-enter coordinates')
+
+  expect(gameboard.placeShip(5, 5, ship4, 'vertical')).toBe(`Error: A ship already occupies this space`)
+
+  gameboard.placeShip(0, 8, new Ship(1), 'horizontal')
+  expect(gameboard.placeShip(0, 7, ship5, 'horizontal')).toBe(`Error: A ship already occupies this space`)
+
+  expect(gameboard.placeShip(2, 5, ship6, 'vertical')).toBe(`Error: A ship already occupies this space`)
 })
