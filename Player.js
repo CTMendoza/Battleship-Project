3980be680b2365e47b32dc97f42d50d66d4fb169
Christopher Mendoza <tiptop1112@gmail.com>
2025-06-05 11:00:51 -0700

    Created new classes to represent Players created two new classes called RealPlayer and ComputerPlayer and passed the Gameboard class to each so that they both have their own gameboard

---
+++ Player.js
@@ -0,0 +1,12 @@
+import { Gameboard } from "./Gameboard";
+class RealPlayer {
+  constructor() {
+    this.gameboard = new Gameboard
+  }
+}
+
+class ComputerPlayer {
+  constructor() {
+    this.gameboard = new Gameboard
+  }
+}
