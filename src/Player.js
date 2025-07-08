996f68114d4dc22169a7f4af6094f4c4d2013032
Christopher Mendoza <tiptop1112@gmail.com>
2025-07-08 11:34:38 -0700

    Added a new method to the ComputerPlayer class Created a new method called getRandomAttackCoordinate() This method allows the computer to generate a random coordinate and checks if the coordinate has been attacked (true) or not (false). If false, the method returns the randomly genereated coordinate to be used when attacking the human player

--- src/Player.js
+++ src/Player.js
@@ -9,6 +9,16 @@ class ComputerPlayer {
   constructor() {
     this.gameboard = new Gameboard
   }
+
+  //method returns a random coordinate that will be used when the computer attacks player board
+  getRandomCoordinate() {
+    let x, y
+    do {
+      x = Math.floor(Math.randon() *10)
+      y = Math.floor(Math.random()* 10)
+    } while (this.gameboard.attacks[x][y])
+   return {x,y}
+  }
 }
 
 export {RealPlayer, ComputerPlayer}
